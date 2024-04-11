911abec68e920222e6ffea43b4a2c348f2572d07
Nero <luizhenriquerezio@gmail.com>
2024-04-11 17:27:06 -0300

    feat: instancia do poetry adicionada

--- app.py
+++ app.py
@@ -4,24 +4,8 @@ from mortar_predict import *
 from home_page import *
 
 def main():
-    st.markdown(
-        """
-        <style>
-        .sidebar .sidebar-content {
-            transition: margin-left .5s, box-shadow .5s;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-    
     st.sidebar.title("Menu")
-    menu_state = st.sidebar.checkbox("Menu", True)
-    if menu_state:
-        st.sidebar.write("Selecione uma página:")
-        page = st.sidebar.radio("", ["Home", "Concrete Predict", "Mortar Predict"])
-    else:
-        page = st.sidebar.radio("Selecione uma página:", ["Home", "Concrete Predict", "Mortar Predict"])
+    page = st.sidebar.radio("Selecione uma página:", ["Home", "Concrete Predict", "Mortar Predict"])
 
     if page == "Home":
         home_page()
